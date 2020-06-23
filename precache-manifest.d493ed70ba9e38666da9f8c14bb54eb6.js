a9985bcd769d435008a6ad92437c1553dd0ee8c0
kamaldobriyalkd <kamal.dobriyal.kd@gmail.com>
2020-06-23 11:43:30 +0530

    Updates

---
+++ precache-manifest.d493ed70ba9e38666da9f8c14bb54eb6.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28a76aa2e9cf1cb4437bdef5dc313ecf",
+    "url": "/tekken/index.html"
+  },
+  {
+    "revision": "508bc25afafbd3fb93a9",
+    "url": "/tekken/static/css/main.3aedaf8b.chunk.css"
+  },
+  {
+    "revision": "3de63e438858553d473b",
+    "url": "/tekken/static/js/2.eee15ee3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tekken/static/js/2.eee15ee3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "508bc25afafbd3fb93a9",
+    "url": "/tekken/static/js/main.544a491f.chunk.js"
+  },
+  {
+    "revision": "08b23227edecbf294445",
+    "url": "/tekken/static/js/runtime-main.1c22dde1.js"
+  },
+  {
+    "revision": "2de86cf4619b76ae5bcf760dfced3668",
+    "url": "/tekken/static/media/Anna Williams.2de86cf4.jpg"
+  },
+  {
+    "revision": "783f62d505680e1b4acd1ea9e9d12f46",
+    "url": "/tekken/static/media/Bryan Fury.783f62d5.jpg"
+  },
+  {
+    "revision": "a65f646d0180d7ca19fe1adc365693c1",
+    "url": "/tekken/static/media/Dr. Bosconovitch.a65f646d.jpg"
+  },
+  {
+    "revision": "53532f0f892da86d50d8cfdccb72c588",
+    "url": "/tekken/static/media/Eddy Gordo.53532f0f.jpg"
+  },
+  {
+    "revision": "7e9466421bf54d9c058a01faf4a41dd4",
+    "url": "/tekken/static/media/Gun Jack.7e946642.jpg"
+  },
+  {
+    "revision": "879b871d33c1cb6d75442660ccbe48f7",
+    "url": "/tekken/static/media/Heihachi Mishima.879b871d.jpg"
+  },
+  {
+    "revision": "448cb49ea82938c5e3efbc01dc9c643d",
+    "url": "/tekken/static/media/Hwoarang.448cb49e.jpg"
+  },
+  {
+    "revision": "3db5e40c4d7664ea61083276e2bb6704",
+    "url": "/tekken/static/media/Jin Kazama.3db5e40c.png"
+  },
+  {
+    "revision": "0a9633bbe38a40a79c313e17ef1c941a",
+    "url": "/tekken/static/media/Julia Chang.0a9633bb.png"
+  },
+  {
+    "revision": "4b557de0ee5a9b0dc1bf7a9b67ce0393",
+    "url": "/tekken/static/media/King.4b557de0.jpg"
+  },
+  {
+    "revision": "3944c40e681b626f63cd6a4f05a5afd8",
+    "url": "/tekken/static/media/Kuma.3944c40e.jpg"
+  },
+  {
+    "revision": "abe510f6e684579effef19bb239eacdc",
+    "url": "/tekken/static/media/Lei Wulong.abe510f6.jpg"
+  },
+  {
+    "revision": "d055a985b3a7d1369dccc7654861ebf3",
+    "url": "/tekken/static/media/Ling Xiaoyu.d055a985.jpg"
+  },
+  {
+    "revision": "be2978bf032f3e0fb978d13d597a6aae",
+    "url": "/tekken/static/media/Marshall Law.be2978bf.jpg"
+  },
+  {
+    "revision": "721178ffc05cf4812c626e246e1a9bfe",
+    "url": "/tekken/static/media/Mokujin.721178ff.jpg"
+  },
+  {
+    "revision": "7fa555686054117b88824b416db9b2be",
+    "url": "/tekken/static/media/Nina Williams.7fa55568.jpg"
+  },
+  {
+    "revision": "e9f0db9d8a9d96331cc173c3faed554b",
+    "url": "/tekken/static/media/Ogre.e9f0db9d.jpg"
+  },
+  {
+    "revision": "ae23964e605b0ea1ddf22950d06dfda6",
+    "url": "/tekken/static/media/Paul Phoenix.ae23964e.jpg"
+  },
+  {
+    "revision": "f051e40c8a04b4d132ae5e4a46a800a4",
+    "url": "/tekken/static/media/True Ogre.f051e40c.jpg"
+  },
+  {
+    "revision": "05ab0b17872870373a71524f58af722c",
+    "url": "/tekken/static/media/Yoshimitsu.05ab0b17.jpg"
+  }
+]);
